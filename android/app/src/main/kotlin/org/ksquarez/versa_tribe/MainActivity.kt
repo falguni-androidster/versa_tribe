9abf773130a4beddd5bd79c5f685f2566e1abcbb
falguni-androidster <falgunimaheta178@gmail.com>
2023-12-22 12:49:52 +0530

    Change in SignIn for Google Auth...

--- android/app/src/main/kotlin/org/ksquarez/versa_tribe/MainActivity.kt
+++ android/app/src/main/kotlin/org/ksquarez/versa_tribe/MainActivity.kt
@@ -1,6 +1,8 @@
 package org.ksquarez.versa_tribe
 
 import io.flutter.embedding.android.FlutterActivity
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 class MainActivity: FlutterActivity() {
 }
--- lib/Screens/sign_in_screen.dart
+++ lib/Screens/sign_in_screen.dart
@@ -1,9 +1,6 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -23,22 +20,11 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
   ConnectivityResult connectivityResult = ConnectivityResult.none;
-
-  //GoogleSignIn googleSignIn = GoogleSignIn(clientId: '801650424679-gfhm5fbk06pbqugfdp8cr1854bo6t46c.apps.googleusercontent.com');
-
-/*  GoogleSignIn googleSignIn = defaultTargetPlatform==TargetPlatform.android?
-       GoogleSignIn(clientId: '924555205284-uo6blsjm998el16rjamv0gnpm9eop1si.apps.googleusercontent.com')
-      :defaultTargetPlatform==TargetPlatform.iOS?GoogleSignIn(clientId: '924555205284-iembjogplf12k21veub6b8a2uqjicbs9.apps.googleusercontent.com'):
-      //:defaultTargetPlatform==TargetPlatform.iOS?GoogleSignIn(clientId: 'com.googleusercontent.apps.924555205284-iembjogplf12k21veub6b8a2uqjicbs9'):
-  GoogleSignIn(clientId: '924555205284-mp8ndh82982lq9h5ak2j5vc81o9to1mi.apps.googleusercontent.com');*/
-
 
   @override
   void initState() {
@@ -57,132 +43,69 @@ class _SignInScreenState extends State<SignInScreen> {
   //   }
   // }
 
-  void _handleSignIn(context) async {
-    //Default definition
-    GoogleSignIn googleSignIn = GoogleSignIn(
-      scopes: [
-        'email',
-      ],
-    );
-
-//If current device is Web or Android, do not use any parameters except from scopes.
-    if (kIsWeb || Platform.isAndroid ) {
-      googleSignIn = GoogleSignIn(
-        scopes: [
-          'email',
-        ],
-      );
-    }
-
-//If current device IOS or MacOS, We have to declare clientID
-//Please, look STEP 2 for how to get Client ID for IOS
-    if (Platform.isIOS || Platform.isMacOS) {
-      googleSignIn = GoogleSignIn(
-        clientId:
-        "924555205284-iembjogplf12k21veub6b8a2uqjicbs9.apps.googleusercontent.com",
-        scopes: [
-          'email',
-        ],
-      );
-    }
+  void _googleSignIn(context) async {
+    debugPrint("------------->GoogleLogin Method Call<------------");
+    //GoogleSignIn googleSignIn = defaultTargetPlatform==TargetPlatform.iOS?GoogleSignIn(clientId: "924555205284-iembjogplf12k21veub6b8a2uqjicbs9.apps.googleusercontent.com"):GoogleSignIn();
+    GoogleSignIn googleSignIn =GoogleSignIn();
+    // //Default definition
+    // GoogleSignIn googleSignIn = GoogleSignIn(
+    //   scopes: [
+    //     'email',
+    //   ],
+    // );
+    //
+    // //If current device is Web or Android, do not use any parameters except from scopes.
+    // if (kIsWeb || Platform.isAndroid ) {
+    //   googleSignIn = GoogleSignIn(
+    //     scopes: [
+    //       'email',
+    //     ],
+    //   );
+    // }
+    //
+    // //If current device IOS or MacOS, We have to declare clientID
+    // //Please, look STEP 2 for how to get Client ID for IOS
+    // if (Platform.isIOS || Platform.isMacOS) {
+    //   googleSignIn = GoogleSignIn(
+    //     clientId:
+    //     "924555205284-iembjogplf12k21veub6b8a2uqjicbs9.apps.googleusercontent.com",
+    //     scopes: [
+    //       'email',
+    //     ],
+    //   );
+    // }
 
     try {
-      googleSignIn.signOut();
-      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+      /// Marks current user as being in the signed out state.
+      await googleSignIn.signOut();
+      debugPrint("Status of Google LogOut------>${await googleSignIn.signOut()}");
+
+      final GoogleSignInAccount? googleLoginAcResult = await googleSignIn.signIn();
+      debugPrint("Status of Google Login------>$googleLoginAcResult");
       // Process the signed-in user if the sign-in was successful
-      if (googleSignInAccount != null) {
-        String id = googleSignInAccount.id;
-        String email = googleSignInAccount.email;
-        String? serverAuthCode = googleSignInAccount.serverAuthCode;
-        String? profilePhoto = googleSignInAccount.photoUrl;
-        String? displayName = googleSignInAccount.displayName;
-        debugPrint("------>id: $id");
-        debugPrint("------>email: $email");
-        debugPrint("------>serverAuthCode: $serverAuthCode");
-        debugPrint("------>profilePhoto: $profilePhoto");
-        debugPrint("------>displayName: $displayName");
+      if (googleLoginAcResult == null) {
+        // User abort the sign-in process
+        showToast(context, "Google Login Cancel......");
+      }else{
+        debugPrint("---->id: ${googleLoginAcResult.id}");
+        debugPrint("---->email: ${googleLoginAcResult.email}");
+        debugPrint("---->serverAuthCode: ${googleLoginAcResult.serverAuthCode}");
+        debugPrint("---->profilePhoto: ${googleLoginAcResult.photoUrl}");
+        debugPrint("---->displayName: ${googleLoginAcResult.displayName}");
 
 
-        GoogleSignInAuthentication googleAuth = await googleSignInAccount.authentication;
-        // Access the Access Token
-        String accessToken = googleAuth.accessToken!;
-        String? idToken = googleAuth.idToken;
-        var runtimeType = googleAuth.runtimeType;
-        int hashCode = googleAuth.hashCode;
-        debugPrint("------>Access Token: $accessToken");
-        debugPrint("------>id Token: $idToken");
-        debugPrint("------>runtimeType: $runtimeType");
-        debugPrint("------>hashCode: $hashCode");
+        GoogleSignInAuthentication googleAuth = await googleLoginAcResult.authentication;
+        debugPrint("Status of Google Auth------->$googleAuth");
+        debugPrint("------>Access Token: ${googleAuth.accessToken!}");
+        debugPrint("------>id Token: ${googleAuth.idToken}");
+        debugPrint("------>runtimeType: ${googleAuth.runtimeType}");
+        debugPrint("------>hashCode: ${googleAuth.hashCode}");
 
         showToast(context, "Google Login Success...");
-      } else {
-        // User cancelled the sign-in process
-        showToast(context, "Google Login Cancel...");
       }
     } catch (error) {
       // Handle the sign-in error
-      debugPrint("Error signing in with Google------*>: $error");
-    }
-   }
-
-
-  // void _handleSignIn(context) async {
-  //   await googleSignIn.signOut();
-  //   googleSignIn = GoogleSignIn(scopes: ['email']);
-  //   try {
-  //     final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
-  //     // Process the signed-in user if the sign-in was successful
-  //     if (googleSignInAccount != null) {
-  //       String id = googleSignInAccount.id;
-  //       String email = googleSignInAccount.email;
-  //       String? serverAuthCode = googleSignInAccount.serverAuthCode;
-  //       String? profilePhoto = googleSignInAccount.photoUrl;
-  //       String? displayName = googleSignInAccount.displayName;
-  //       debugPrint("------>id: $id");
-  //       debugPrint("------>email: $email");
-  //       debugPrint("------>serverAuthCode: $serverAuthCode");
-  //       debugPrint("------>profilePhoto: $profilePhoto");
-  //       debugPrint("------>displayName: $displayName");
-  //
-  //
-  //       GoogleSignInAuthentication googleAuth = await googleSignInAccount.authentication;
-  //       // Access the Access Token
-  //       String accessToken = googleAuth.accessToken!;
-  //       String? idToken = googleAuth.idToken;
-  //       var runtimeType = googleAuth.runtimeType;
-  //       int hashCode = googleAuth.hashCode;
-  //       debugPrint("------>Access Token: $accessToken");
-  //       debugPrint("------>id Token: $idToken");
-  //       debugPrint("------>runtimeType: $runtimeType");
-  //       debugPrint("------>hashCode: $hashCode");
-  //
-  //       showToast(context, "Google Login Success...");
-  //     } else {
-  //       // User cancelled the sign-in process
-  //       showToast(context, "Google Login Cancel...");
-  //     }
-  //   } catch (error) {
-  //     // Handle the sign-in error
-  //     debugPrint("Error signing in with Google------*>: $error");
-  //   }
-  // }
-
-  Future<void> _loginWithFacebook() async {
-    try {
-      final LoginResult result = await FacebookAuth.instance.login();
-
-      if (result.status == LoginStatus.success) {
-        // Access token obtained
-        final AccessToken accessToken = result.accessToken!;
-        debugPrint('Access Token: ${accessToken.token}');
-        // Use the access token for further operations
-      } else {
-        // Login failed or was canceled by the user
-        debugPrint('Facebook login failed');
-      }
-    } catch (e) {
-      // Handle exceptions
-      debugPrint('Error logging in with Facebook: $e');
+      debugPrint("Google Login Error----------*>: $error");
     }
   }
 
@@ -377,7 +300,7 @@ class _SignInScreenState extends State<SignInScreen> {
                             padding: const EdgeInsets.all(8.0),
                             child: ElevatedButton.icon(
                               onPressed: () {
-                                _handleSignIn(context);
+                                _googleSignIn(context);
                               },
                               icon: Image.asset(ImagePath.googlePath),
                               label: const Text(CustomString.google,
@@ -395,7 +318,7 @@ class _SignInScreenState extends State<SignInScreen> {
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: ElevatedButton.icon(
-                              onPressed: _loginWithFacebook,
+                              onPressed: (){},//_loginWithFacebook,
                               icon: Image.asset(ImagePath.facebookPath),
                               label: const Text(CustomString.facebook,
                                   style: TextStyle(
