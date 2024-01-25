344a76e3cc3c4c9344bb6b34aaa570c417f88846
falguni-androidster <falgunimaheta178@gmail.com>
2024-01-25 17:36:22 +0530

    Ongoing Project ->Correction in card which display already applayed or joined.

--- lib/Screens/sign_in_screen.dart
+++ lib/Screens/sign_in_screen.dart
@@ -1,7 +1,5 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:versa_tribe/Screens/sign_up_screen.dart';
 import '../Utils/auth_options.dart';
