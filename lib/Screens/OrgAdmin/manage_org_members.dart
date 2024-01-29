c58ccf9c300f2927c8aa7697b4a23302c923c349
falguni-androidster <falgunimaheta178@gmail.com>
2024-01-29 19:19:31 +0530

    basic changes related manage_org_member

--- lib/Screens/OrgAdmin/manage_org_members.dart
+++ lib/Screens/OrgAdmin/manage_org_members.dart
@@ -26,6 +26,15 @@ class _ManageOrgMembersState extends State<ManageOrgMembers> with SingleTickerPr
     super.initState();
   }
 
+  // Call this when the user pull down the screen
+    Future<void> _loadData() async {
+      try {
+        ApiConfig.getOrgMemberData(context: context,orgName: widget.orgNAME, tabIndex: 0);
+        ApiConfig.getOrgMemberData(context: context,orgName: widget.orgNAME, tabIndex: 1);
+      } catch (err) {
+        rethrow;
+      }
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -44,52 +53,55 @@ class _ManageOrgMembersState extends State<ManageOrgMembers> with SingleTickerPr
         title: const Text(CustomString.manageOrgMember, style: TextStyle(color: CustomColors.kBlueColor,fontSize: 16, fontFamily: 'Poppins')),
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: size.height * 0.02, left: size.width * 0.02, right: size.height * 0.02),
-            child: Consumer<IndexProvider>(builder: (context, val, child) {
-              return TabBar(
-                isScrollable: true,
-                labelPadding: const EdgeInsets.symmetric(horizontal: 5),
-                controller: _tabController,
-                indicator: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: CustomColors.kBlueColor, // Change the color of the selected tab here
-                ),
-                indicatorSize: TabBarIndicatorSize.label,
-                unselectedLabelStyle: const TextStyle(fontSize: 14, color: CustomColors.kBlackColor, fontWeight: FontWeight.w400, fontFamily: 'Poppins'),
-                tabAlignment: TabAlignment.start,
-                labelStyle: const TextStyle(fontSize: 14, color : CustomColors.kWhiteColor,fontWeight: FontWeight.w400, fontFamily: 'Poppins'),
-                tabs: <Widget>[
-                  Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Text(CustomString.pendingRequested)
+      body: RefreshIndicator(
+        onRefresh: _loadData,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: size.height * 0.02, left: size.width * 0.02, right: size.height * 0.02),
+              child: Consumer<IndexProvider>(builder: (context, val, child) {
+                return TabBar(
+                  isScrollable: true,
+                  labelPadding: const EdgeInsets.symmetric(horizontal: 5),
+                  controller: _tabController,
+                  indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: CustomColors.kBlueColor, // Change the color of the selected tab here
                   ),
-                  Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Text(CustomString.approved)
-                  ),
-                ],
-              );
-            }),
-          ),
-          Expanded(
-            child: TabBarView(
-              controller: _tabController,
-              physics: const NeverScrollableScrollPhysics(),
-              children: <Widget>[
-
-                ///Pending Requested
-                PendingRequestedOrgMembersScreen(orgNAME: widget.orgNAME, orgID: widget.orgID),
-
-                ///Approved
-                ApprovedMemberScreen(orgNAME: widget.orgNAME, orgID: widget.orgID)
-              ],
+                  indicatorSize: TabBarIndicatorSize.label,
+                  unselectedLabelStyle: const TextStyle(fontSize: 14, color: CustomColors.kBlackColor, fontWeight: FontWeight.w400, fontFamily: 'Poppins'),
+                  tabAlignment: TabAlignment.start,
+                  labelStyle: const TextStyle(fontSize: 14, color : CustomColors.kWhiteColor,fontWeight: FontWeight.w400, fontFamily: 'Poppins'),
+                  tabs: <Widget>[
+                    Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: const Text(CustomString.pendingRequested)
+                    ),
+                    Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: const Text(CustomString.approved)
+                    ),
+                  ],
+                );
+              }),
             ),
-          ),
-        ],
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                physics: const NeverScrollableScrollPhysics(),
+                children: <Widget>[
+
+                  ///Pending Requested
+                  PendingRequestedOrgMembersScreen(orgNAME: widget.orgNAME, orgID: widget.orgID),
+
+                  ///Approved
+                  ApprovedMemberScreen(orgNAME: widget.orgNAME, orgID: widget.orgID)
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
